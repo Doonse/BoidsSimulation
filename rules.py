589037429b41c1b0223259bb82fc44185733a063
Daniel <danielius.s2601@gmail.com>
2023-02-15 22:34:52 +0100

    feat: template rules

---
+++ rules.py
@@ -0,0 +1,40 @@
+
+
+class Rules:
+    # Rules for the boids to follow
+    def __init__(self, flock):
+        self.boids = flock # Flock of boids 
+
+
+    def cohesion(self, boid):
+        # Boids move towards the center of mass of neighboring boid
+        # Find the center of mass of the flock
+        # Move towards the center of mass
+        pass
+
+    def separation(self, boid):
+        # Boids avoid crowding local flockmates
+        # Find the distance between the boid and its neighbors
+        # If the distance is less than a certain threshold, move away from the neighbor
+        pass
+
+    def alignment(self, boid):
+        # Boids try to match velocity with near boids
+        # Find the average velocity of the flock
+        # Move towards the average velocity
+        pass
+
+    def update(self):
+        # Update the boids
+        pass
+
+
+
+
+
+    
+
+
+    
+
+
