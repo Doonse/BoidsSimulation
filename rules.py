80c0e0257b78a2819cf633525e304ddc90bd2783
Daniel <danielius.s2601@gmail.com>
2023-02-16 16:20:39 +0100

    chore: refactor

--- rules.py
+++ rules.py
@@ -2,18 +2,18 @@
 from pygame import Vector2 as Vector
 
 class Rules:
-    def __init__(self, boids):
-        self.boids = boids
-        self.cohesion = 0.01
-        self.separation = 0.1
-        self.alignment = 0.1
-
-    def cohesion(self, boids): # boids is a list of boids
-        center = Vector(0, 0) # center is a vector
-        for boid in boids: # boid in boids
-            center += boid.position # add the position of the boid to the center vector
-        center /= len(boids) # divide the center vector by the number of boids
-        return 0.2 * (center - self.position) / 1000 # return the vector from the center of the boids to the boid
+    def __init__(self, x, y, width, height):
+        self.position = Vector(x, y)
+        self.velocity = Vector(0, 0)
+        self.width = width
+        self.height = height
+    
+    def cohesion(self, flock, range=70):
+        center = Vector(0, 0)
+        for boid in flock:
+            center += boid.position
+        center /= len(flock)
+        return 0.2 * (center - self.position) / 100
 
     def separation(self, boids):
         distance = Vector(0, 0)
