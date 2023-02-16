bade2131660b2c6ce363941cd70b66cc19ba5007
Daniel <danielius.s2601@gmail.com>
2023-02-16 17:47:54 +0100

    build

--- boid.py
+++ boid.py
@@ -7,16 +7,20 @@ class Boid:
     def __init__(self, x, y, width, height):
         self.position = Vector(x, y)
         self.velocity = Vector(random.uniform(-1, 1), random.uniform(-1, 1))
+        self.acceleration = Vector(0, 0)
         self.width = width
         self.height = height
+        self.max_speed = 5
+        self.max_force = 0.1
 
     def draw(self, screen):
         pg.draw.circle(screen, (255, 255, 255), self.position, 5)
 
     def update(self, boids):
+        self.acceleration += Rules.cohesion(self, boids) + Rules.separation(self, boids) + Rules.alignment(self, boids)
+        self.velocity += self.acceleration
+        self.velocity.scale_to_length(5)
         self.position += self.velocity
-        self.velocity += Rules.cohesion(self, boids) + Rules.separation(self, boids) + Rules.alignment(self, boids)
-        self.velocity.scale_to_length(2)
     
     def edge_wrap(self, width, height):
         if self.position.x > width:
