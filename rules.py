e407cac7f7b74d1102c06d435248d19d868acd8e
Daniel <danielius.s2601@gmail.com>
2023-02-16 17:48:45 +0100

    feat: rules for boids

--- rules.py
+++ rules.py
@@ -1,32 +1,69 @@
-
+import numpy as np
 from pygame import Vector2 as Vector
+import random
 
 class Rules:
     def __init__(self, x, y, width, height):
         self.position = Vector(x, y)
-        self.velocity = Vector(0, 0)
+        self.velocity = Vector(random.uniform(-1, 1), random.uniform(-1, 1))
         self.width = width
         self.height = height
     
-    def cohesion(self, flock, range=70):
-        center = Vector(0, 0)
-        for boid in flock:
-            center += boid.position
-        center /= len(flock)
-        return 0.2 * (center - self.position) / 100
+    def cohesion(self, boids):
+        turn = Vector(*np.zeros(2))
+        m = 0
+        center_of_mass = Vector(*np.zeros(2))
+        for boid in boids:
+            if np.linalg.norm(boid.position - self.position) < 50:
+                center_of_mass += boid.position
+                m += 1
+        if m > 0:
+            center_of_mass /= m
+            center_of_mass = Vector(*center_of_mass)
+            vec_to_com = center_of_mass - self.position
+            if np.linalg.norm(vec_to_com) > 0:
+                vec_to_com = (vec_to_com / np.linalg.norm(vec_to_com)) * self.max_speed
+            turn = vec_to_com - self.velocity
+            if np.linalg.norm(turn)> self.max_force:
+                turn = (turn /np.linalg.norm(turn)) * self.max_force
+
+        return turn
 
     def separation(self, boids):
-        distance = Vector(0, 0)
+        turn = Vector(*np.zeros(2))
+        m = 0
+        average = Vector(*np.zeros(2))
         for boid in boids:
-            if boid.position != self.position:
-                distance += 0.1 * (self.position - boid.position) / (self.position.distance_to(boid.position) ** 4)
-        return distance
+            distance = np.linalg.norm(boid.position - self.position)
+            if self.position != boid.position and distance < 50:
+                diff = self.position - boid.position
+                diff /= distance
+                average += diff
+                m += 1
+        if m > 0:
+            average /= m
+            average = Vector(*average)
+            if np.linalg.norm(turn) > 0:
+                avg_vector = (average / np.linalg.norm(turn)) * self.max_speed
+            turn = average - self.velocity
+            if np.linalg.norm(turn)> self.max_force:
+                steering = (turn /np.linalg.norm(turn)) * self.max_force
+
+        return turn
 
     def alignment(self, boids):
-        velocity = Vector(0, 0) 
+        turn = Vector(*np.zeros(2))
+        m = 0
+        average = Vector(*np.zeros(2))
         for boid in boids:
-            velocity += boid.velocity
-        velocity /= len(boids)
-        return  0.1 * (velocity - self.velocity) / 8
-    
+            if np.linalg.norm(boid.position - self.position) < 50:
+                average += boid.velocity
+                m += 1
+        if m > 0:
+            average /= m
+            average = Vector(*average)
+            average = (average /np.linalg.norm(average)) * self.max_speed
+            turn = average - self.velocity
+
+        return turn
 
