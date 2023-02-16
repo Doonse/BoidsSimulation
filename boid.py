27ce7980dede405ea66a17a244ff97010b4ab3d2
Danielius Stanevicius <daniel@tromso-stud2-186.wifi.uit.no>
2023-02-16 11:53:31 +0100

    redo

--- boid.py
+++ boid.py
@@ -1,40 +1,57 @@
-import numpy as np
 import pygame as pg
+from pygame import Vector2 as Vector
+import random
+import math
 
 
 class Boid:
+    def __init__(self, x, y, width, height):
+        self.position = Vector(x, y)
+        self.velocity = Vector(random.uniform(-1, 1), random.uniform(-1, 1))
+        self.width = width
+        self.height = height
+
+    def draw(self, screen):
+        pg.draw.circle(screen, (255, 255, 255), self.position, 5)
     
-    def __init__(self, position, velocity, acceleration, max_speed, max_force):
-        self.position = np.array(position, dtype=np.float64)
-        self.velocity = np.array(velocity, dtype=np.float64)
-        self.acceleration = np.array(acceleration, dtype=np.float64) 
-        self.max_speed = max_speed
-        self.max_force = max_force
+    def cohesion(self, boids): # boids is a list of boids
+        center = Vector(0, 0) # center is a vector
+        for boid in boids: # boid in boids
+            center += boid.position # add the position of the boid to the center vector
+        center /= len(boids) # divide the center vector by the number of boids
+        return (center - self.position) / 1000 # return the vector from the center of the boids to the boid
+
+    def separation(self, boids):
+        distance = Vector(0, 0)
+        for boid in boids:
+            if boid.position != self.position:
+                distance += (self.position - boid.position) / (self.position.distance_to(boid.position) ** 4)
+        return distance
+
+    def alignment(self, boids):
+        velocity = Vector(0, 0) 
+        for boid in boids:
+            velocity += boid.velocity
+        velocity /= len(boids)
+        return (velocity - self.velocity) / 8
+    
+    def edge_wrap(self, width, height):
+        if self.position.x > width:
+            self.position.x = 0
+        elif self.position.x < 0:
+            self.position.x = width
+        if self.position.y > height:
+            self.position.y = 0
+        elif self.position.y < 0:
+            self.position.y = height
+        
+
+    def update(self, boids):
+        self.position += self.velocity
+        self.velocity += self.cohesion(boids) + self.separation(boids) + self.alignment(boids)
+        self.velocity.scale_to_length(2)
+    
+
 
     
-    def update(self, boids, alignment_weight, cohesion_weight, separation_weight, obstacle_weight, obstacles):
-        pass
-
-    def flockmates(self, boids, distance):
-        return [boid for boid in boids if np.linalg.norm(boid.position - self.position) < distance and boid != self]
     
-
-
-    # Rules class ---
-    def alignment_rule(self, flockmates, weight):
-        pass
-
-    def cohesion_rule(self, flockmates, weight):
-        pass
-
-    def separation_rule(self, flockmates, weight):
-        pass
-
-    def obstacle_rule(self, obstacles, weight):
-        pass
-    # Rules class ---
-
-    def apply_limits(self):
-        self.velocity = self.velocity.limit(self.max_speed)
-        self.acceleration = self.acceleration.limit(self.max_force)
-        self.position = self.position.wrap()
--- main.py
+++ main.py
@@ -1,43 +1,31 @@
 import pygame as pg
+import random 
+import numpy as np
 from boid import Boid
-from flock import Flock
 
-SCREEN_WIDTH = 800 # Width of the screen
-SCREEN_HEIGHT = 600 # Height of the screen 
-NUM_BOIDS = 50 # Number of boids in the simulation 
-BOID_RADIUS = 5 # Radius of the boids
-BOID_SPEED = 3 # Speed of the boids
-BOID_ACCELERATION = 0.1 # Acceleration of the boids
-ALIGNMENT_WEIGHT = 1.0 # Weight of the alignment rule
-COHESION_WEIGHT = 1.0 # Weight of the cohesion rule
-SEPARATION_WEIGHT = 1.5 # Weight of the separation rule
-AVOIDANCE_WEIGHT = 2.0 # Weight of the avoidance rule
+WIDTH = 800
+HEIGHT = 600
 
-def main():
-    # initialize pygame and screen with borders
+def main(WIDTH, HEIGHT):
     pg.init()
-    screen = pg.display.set_mode((800, 600))
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    screen.fill("BLACK")
     pg.display.set_caption("Boids")
     clock = pg.time.Clock()
+    boids = [Boid(WIDTH - 100, HEIGHT - 100, 10, 10) for i in range(100)] # 40 boids
     running = True
-
-    # create a flock of boids
-    flock = Flock(400, 300)
-    for i in range(10):
-        flock.addBoid(Boid(400, 300))
-
-    # main loop
     while running:
-        clock.tick(60)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
-
-        # update and draw
         screen.fill((0, 0, 0))
-        flock.update()
-        flock.draw(screen)
+        for boid in boids:
+            boid.update(boids)
+            boid.draw(screen)
+            boid.edge_wrap(WIDTH, HEIGHT)
         pg.display.flip()
+        clock.tick(60)
+    pg.quit()
 
 if __name__ == "__main__":
-    main()
+    main(WIDTH, HEIGHT)
