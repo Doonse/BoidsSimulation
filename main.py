27d5c95c09654c633975d41d2e40e5680dc23b36
Daniel <danielius.s2601@gmail.com>
2023-02-17 10:25:08 +0100

    chore: Commented and structured

--- main.py
+++ main.py
@@ -8,22 +8,48 @@ HEIGHT = 600
 
 
 def main(WIDTH, HEIGHT):
+
+    # Initialize pygame
     pg.init()
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     screen.fill("BLACK")
     pg.display.set_caption("Boids")
     clock = pg.time.Clock()
-    boids = [Boid(random.randrange(0, 800), random.randrange(0, 800), 1, 1) for i in range(20)]
+
+
+    # Initialize boids with random positions as explained in pseudocode http://www.kfish.org/boids/pseudocode.html
+    # Random velociies are assigned in the Boid class as well
+    # Acceleration vector is initialized to 0
+    # Rest is constant in the Boid class except for the width and height, which are given as an argument
+    boids = [Boid(random.randrange(0, 800), random.randrange(0, 800), 1, 1) for i in range(100)] 
+
+
+    # Main loop
     running = True
     while running:
+        
+        # Event loop
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
         screen.fill((0, 0, 0))
+
+        # Boid loop
         for boid in boids:
-            boid.update(boids)
+            
+            # Draw boids on screen 
             boid.draw(screen)
+            
+            # Update boids position and velocity 
+            # This calls the update function in the Boid class, which consists of the three rules
+            # The three rules are called from the Rules class
+            # The three rules are cohesion, separation and alignment
+            boid.update(boids)
+
+            # Wrap boids around the screen if they go out of bounds
             boid.edge_wrap(WIDTH, HEIGHT)
+
+        # Update the screen
         pg.display.flip()
         clock.tick(60)
     pg.quit()
