9f4eb04f24df722d76229c46362146905da22e02
Daniel <danielius.s2601@gmail.com>
2023-02-17 11:29:43 +0100

    fix: went back

--- rules.py
+++ rules.py
@@ -8,29 +8,32 @@ class Rules:
         self.velocity = Vector(random.uniform(-1, 1), random.uniform(-1, 1))
 
     # Implementing the first rule: (b_1.position + b_2.position + ... + b_{J-1}.position + b_{J+1}.position + ... + b_N.position) / (N-1)
-    def fly_towards_center(self, boid):
+    def fly_towards_center(self, boids):
 
         # Weight of the rule
         weight = 0.5
         
         # Initialize center vector
         center = Vector(0, 0) 
-    
-        # If the boid is not the boid we are calculating the rule for   
-        if boid.position != self.position: 
 
-            # Add the position of the boid to the center vector
-            center += boid.position 
+        # Loop through all boids
+        for boid in boids: 
+
+            # If the boid is not the boid we are calculating the rule for   
+            if boid.position != self.position: 
+
+                # Add the position of the boid to the center vector
+                center += boid.position 
 
         # Divide the center vector by the number of boids minus one
-        center = center / (99) 
+        center = center / (len(boids) - 1) 
 
         # Return the rule multiplied by weight of the rule
         return weight * (center - self.position) / 100 
 
 
     # Implementing second rule, which makes sure boids don't collide with each other
-    def keep_distance_away(self, boid):
+    def keep_distance_away(self, boids):
 
         # Weight of the rule
         weight = 0.3
@@ -38,34 +41,40 @@ class Rules:
         # Initialize distance vector
         distance = Vector(0, 0)
 
-        # If the boid is not the boid we are calculating the rule for
-        if boid.position != self.position:
-            
-            # If the distance between the boid and the boid we are calculating the rule for is less than 100
-            if self.position.distance_to(boid.position) < 10:
+        # Loop through all boids
+        for boid in boids:
+                
+                # If the boid is not the boid we are calculating the rule for
+                if boid.position != self.position:
                     
-                    # Add the distance vector to the distance vector
-                    distance += weight * (self.position - boid.position)
+                    # If the distance between the boid and the boid we are calculating the rule for is less than 100
+                    if self.position.distance_to(boid.position) < 10:
+                            
+                            # Add the distance vector to the distance vector
+                            distance += weight * (self.position - boid.position)
 
         # Return the distance vector
         return distance
     
 
     # Implementing the third rule, which adjusts the velocity of the boids to match the velocity of the boids around them
-    def match_velocity(self, boid):
+    def match_velocity(self, boids):
             
             # Weight of the rule
-            weight = 0.4
+            weight = 0.3
     
             # Initialize velocity vector
             velocity = self.velocity
-
-            # If the boid is not the boid we are calculating the rule for
-            if boid.position != self.position:
-                velocity += boid.velocity
+    
+            # Loop through all boids
+            for boid in boids:
+    
+                # If the boid is not the boid we are calculating the rule for
+                if boid.position != self.position:
+                    velocity += boid.velocity
 
             # Divide the velocity vector by the number of boids minus one
-            velocity = velocity / (99)
+            velocity = velocity / (len(boids) - 1)
 
             # Return the rule multiplied by weight of the rule
             return weight * (velocity - self.velocity) / 8
