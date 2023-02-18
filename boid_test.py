b4f5809128d27b1d94e0e379b2863d3299cf9079
Daniel <danielius.s2601@gmail.com>
2023-02-18 13:29:44 +0100

    error: somthing wrong with separation

--- boid_test.py
+++ boid_test.py
@@ -7,23 +7,35 @@ class Boid:
     def __init__(self, x, y):
         self.position = Vector(x, y)
         self.velocity = Vector(random.uniform(-1, 1), random.uniform(-1, 1))
+        self.radius = 70
 
     # Draw the boids on the screen
     def draw(self, screen):
         pg.draw.circle(screen, (255, 255, 255), self.position, 5)
 
-    def fly_towards_center(self, boids):
-        perceived_center = Vector(0, 0)
+    def neighbors(self, boids):
+        neighbors = []
 
         for boid in boids:
-            if boid.position != self.position: 
-                perceived_center += boid.position 
-        perceived_center = perceived_center / 100 
-        return (perceived_center - self.position) / 1000
+            if boid.position != self.position:
+                if self.position.distance_to(boid.position) < self.radius:
+                    neighbors.append(boid)
+        return neighbors
 
+
+    def fly_towards_center(self, boids):
+        center = Vector(0, 0)
+        for boid in boids:
+            if boid.position != self.position:
+                center += boid.position
+        
+        center = center / 100
+        print(center)
+        return (center - self.position) / 10
+    
 
     def keep_distance_away(self, boids):
-        distance = Vector(0, 0)
+        distance = Vector(1000, 0)
         for boid in boids:
             if boid.position != self.position:
                 if self.position.distance_to(boid.position) < 10:
@@ -44,7 +56,7 @@ class Boid:
 
         for boid in boids:
             if boid.position != self.position: 
-                perceived_center += boid.position 
+                perceived_center += boid.position
         perceived_center = perceived_center / 100 
         return (perceived_center - self.position) / 1000
 
@@ -58,29 +70,23 @@ class Boid:
         if self.position.y < 0:
             self.position.y = 900
 
+
     # This is where the three rules are called, which move the boids
     def update(self, boids):
         # Weights of the rules
-        w1 = 1 # Rule1: Move towards the center of mass of neighbours
-        w2 = 0.1 # Rule2: Keep a small distance away from other objects 
-        w3 = 0.3 # Rule3: Try to match velocity with near boids
-        w4 = 1 # Rule4: Tend to the palace
+        w1 = 0.4 # Rule1: Move towards the center of mass of neighbours
+        w2 = 0.2 # Rule2: Keep a small distance away from other objects 
+        w3 = 0.2 # Rule3: Try to match velocity with near boids
+        w4 = 0 # Rule4: Tend to the palace
 
         # Find the center of the boids before applying the rules 
+        n = self.neighbors(boids)
+        cohesian = w1 * self.fly_towards_center(n)
+        separation = w2 * self.keep_distance_away(n)
+        alignment = w3 * self.match_velocity(n)
 
-        self.velocity += self.fly_towards_center(boids) * w1  + self.keep_distance_away(boids) * w2 + self.match_velocity(boids) * w3 + self.tend_to_place(boids) * w4
+        self.velocity = self.velocity + cohesian + separation # + alignment
 
         self.velocity.scale_to_length(5)
         self.position += self.velocity
         self.wrap_position(boids)
-    
-
-
-
-
-
-    # Anti-flocking behaviour
-    # Negate the first rule (moving towards the centre of mass of neighbours)
-    def anti_flock(self, boids):
-        pass
-
