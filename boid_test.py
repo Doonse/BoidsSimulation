074150e2342d4ef5b10f2946f8aa091486c325ea
Daniel <danielius.s2601@gmail.com>
2023-02-18 14:30:42 +0100

    error: local minima achieved, fixing

--- boid_test.py
+++ boid_test.py
@@ -1,12 +1,12 @@
 import pygame as pg
-from pygame import Vector2 as Vector
+from pygame import Vector2
 import random
 from rules import Rules
 
 class Boid:
-    def __init__(self, x, y):
-        self.position = Vector(x, y)
-        self.velocity = Vector(random.uniform(-1, 1), random.uniform(-1, 1))
+    def __init__(self, screen_width, screen_height):
+        self.position = Vector2(random.uniform(0, screen_width), random.uniform(0, screen_height))
+        self.velocity = Vector2(random.uniform(-1, 1), random.uniform(-1, 1))
         self.radius = 70
 
     # Draw the boids on the screen
@@ -15,44 +15,42 @@ class Boid:
 
     def neighbors(self, boids):
         neighbors = []
-
         for boid in boids:
             if boid.position != self.position:
                 if self.position.distance_to(boid.position) < self.radius:
                     neighbors.append(boid)
         return neighbors
 
-
     def fly_towards_center(self, boids):
-        center = Vector(0, 0)
+        center = Vector2(0, 0)
         for boid in boids:
             if boid.position != self.position:
                 center += boid.position
-        
-        center = center / 100
-        print(center)
-        return (center - self.position) / 10
+        if len(boids) > 1:
+            center = center / (len(boids) - 1)
+            return (center - self.position) / 100
+        return Vector2(0, 0)
     
-
     def keep_distance_away(self, boids):
-        distance = Vector(1000, 0)
+        distance = Vector2()
         for boid in boids:
             if boid.position != self.position:
-                if self.position.distance_to(boid.position) < 10:
+                if (boid.position - self.position).length() < 10:
                     distance = distance - (boid.position - self.position)
         return distance
     
-
     def match_velocity(self, boids):
-        velocity = Vector(0, 0)
+        velocity = Vector2(0, 0)
         for boid in boids:
             if boid.position != self.position:
                 velocity += boid.velocity
-        velocity = velocity / 100
-        return (velocity - self.velocity) / 8
+        if len(boids) > 1:
+            velocity = velocity / (len(boids) - 1)
+            return (velocity - self.velocity) / 8
+        return Vector2(0, 0)
 
     def tend_to_place(self, boids):
-        perceived_center = Vector(0, 0)
+        perceived_center = Vector2(0, 0)
 
         for boid in boids:
             if boid.position != self.position: 
@@ -60,23 +58,22 @@ class Boid:
         perceived_center = perceived_center / 100 
         return (perceived_center - self.position) / 1000
 
-    def wrap_position(self, boids):
-        if self.position.x > 1200:
+    def wrap_position(self, screen_width, screen_height):
+        if self.position.x > screen_width:
             self.position.x = 0
         if self.position.x < 0:
-            self.position.x = 1200
-        if self.position.y > 900:
+            self.position.x = screen_width
+        if self.position.y > screen_height:
             self.position.y = 0
         if self.position.y < 0:
-            self.position.y = 900
-
+            self.position.y = screen_height
 
     # This is where the three rules are called, which move the boids
-    def update(self, boids):
+    def update(self, boids, screen_width, screen_height):
         # Weights of the rules
-        w1 = 0.4 # Rule1: Move towards the center of mass of neighbours
+        w1 = 0.3 # Rule1: Move towards the center of mass of neighbours
         w2 = 0.2 # Rule2: Keep a small distance away from other objects 
-        w3 = 0.2 # Rule3: Try to match velocity with near boids
+        w3 = 0.4 # Rule3: Try to match velocity with near boids
         w4 = 0 # Rule4: Tend to the palace
 
         # Find the center of the boids before applying the rules 
@@ -85,8 +82,8 @@ class Boid:
         separation = w2 * self.keep_distance_away(n)
         alignment = w3 * self.match_velocity(n)
 
-        self.velocity = self.velocity + cohesian + separation # + alignment
+        self.velocity = self.velocity + cohesian + separation + alignment 
 
         self.velocity.scale_to_length(5)
         self.position += self.velocity
-        self.wrap_position(boids)
+        self.wrap_position(screen_width, screen_height)
