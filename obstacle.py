7750143d3a5d03d442c00cda964106eebc2e1fe3
Daniel <danielius.s2601@gmail.com>
2023-02-25 03:07:01 +0100

    feat: size

--- obstacle.py
+++ obstacle.py
@@ -10,6 +10,7 @@ class Obstacle:
     def __init__(self, screen_width, screen_height, margin=100):
         self.position = Vector2(random.randrange(margin, screen_width - margin), random.randrange(margin, screen_height - margin))
         self.radius = 10
+        self.size = self.radius
 
     def draw(self, screen):
         pygame.draw.ellipse(screen, (0, 100, 200), (self.position.x - self.radius, self.position.y - self.radius, self.radius * 2, self.radius * 2), self.radius)
